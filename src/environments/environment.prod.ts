9c0a4da64ce5cc93071c6260f093e9537de3d63a
Andrew Bateman <gomezbateman@yahoo.com>
2020-01-23 20:37:23 +0100

    db access info removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDVb032AZmbEE0SHPS7AI3GdKQaq-e6B8c",
-    authDomain: "angular-blog-14cef.firebaseapp.com",
-    databaseURL: "https://angular-blog-14cef.firebaseio.com",
-    projectId: "angular-blog-14cef",
-    storageBucket: "angular-blog-14cef.appspot.com",
-    messagingSenderId: "331826448119",
-    appId: "1:331826448119:web:274c208e071ac64eee75f2",
-    measurementId: "G-GP9RVZ6C41"
+
 	}
 };
