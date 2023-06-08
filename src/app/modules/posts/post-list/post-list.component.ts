b5439627297a553dc91dc738419ff379a19a6a65
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-08 21:18:28 +0200

    refactor: use breakpoints and css grid for posts

--- src/app/modules/posts/post-list/post-list.component.ts
+++ src/app/modules/posts/post-list/post-list.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy, inject } from "@angular/core";
 import { Meta, Title } from "@angular/platform-browser";
 import { Router, NavigationExtras } from "@angular/router";
 
 import { Post } from "../post.model";
 import { PostService } from "../post-services/post.service";
 import { Observable } from "rxjs";
+import { BreakpointService } from "../../../core/breakpoint.service";
 
 @Component({
   selector: "app-post-list",
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.scss"]
 })
 export class PostListComponent implements OnInit {
   title = "Blog Posts";
   posts$: Observable<Post[]>;
+  breakpointService = inject(BreakpointService);
+  columns$ = this.breakpointService.columns$;
 
   constructor(
     private postService: PostService,
