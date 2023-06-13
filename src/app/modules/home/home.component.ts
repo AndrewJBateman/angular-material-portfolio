9cd83efb81d676be884459befda128031a7279be
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-13 12:14:14 +0200

    styles: improve grid layout & card padding

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -8,7 +8,8 @@ import { Area } from "./area.model";
 @Component({
   selector: "app-home",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: "./home.component.html"
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   firestoreDataService = inject(FirestoreDataService);
