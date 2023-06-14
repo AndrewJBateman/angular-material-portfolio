d2d02b471027766b2e4ffa3d6a81ce15e472e8f2
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-14 16:54:10 +0200

    feat: add sorting to firebase data service

--- src/app/core/services/firestore-data.service.ts
+++ src/app/core/services/firestore-data.service.ts
@@ -1,20 +1,26 @@
-import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { Project } from 'src/app/modules/projects/project.model';
+import { Injectable, inject } from "@angular/core";
+import { Firestore, collection, collectionData } from "@angular/fire/firestore";
+import { Observable, map } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FirestoreDataService {
   data$: Observable<any[]> = new Observable<any[]>();
   firestore: Firestore = inject(Firestore);
   id: number = 0;
 
-  // get array collection Observable
+  // get array collection Observable and sort data by id (number or string)
   getData(collectionName: string): Observable<any[]> {
     const data = collection(this.firestore, collectionName);
-    this.data$ = collectionData(data) as Observable<any[]>;
+    const unsortedData$ = collectionData(data) as Observable<any[]>;
+    this.data$ = unsortedData$.pipe(
+      map((items) => items.sort(this.sortById)),
+    );
     return this.data$;
+  }
+
+  sortById(a: any, b: any) {
+    return a.id < b.id ? -1 : 1;
   }
 }
