b45066d6f82b4da89f24f21c076a7a794dccb83a
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-17 22:25:16 +0200

    refactor: add sort by date publ. to firestore srvc

--- src/app/core/services/firestore-data.service.ts
+++ src/app/core/services/firestore-data.service.ts
@@ -10,17 +10,25 @@ export class FirestoreDataService {
   firestore: Firestore = inject(Firestore);
   id: number = 0;
 
-  // get array collection Observable and sort data by id (number or string)
+  // get array collection from Firestore const {second} =
+  // If collection items had an id then sort data by id (number or string)
+  // Otherwise sort items by published date (only applies to posts)
   getData(collectionName: string): Observable<any[]> {
     const data = collection(this.firestore, collectionName);
     const unsortedData$ = collectionData(data) as Observable<any[]>;
     this.data$ = unsortedData$.pipe(
-      map((items) => items.sort(this.sortById)),
+      map((items) =>
+        items[0].id ? items.sort(this.sortById) : items.sort(this.sortByDate)
+      )
     );
     return this.data$;
   }
 
   sortById(a: any, b: any) {
     return a.id < b.id ? -1 : 1;
+  }
+
+  sortByDate(a: any, b: any) {
+    return a.published.seconds > b.published.seconds ? -1 : 1;
   }
 }
