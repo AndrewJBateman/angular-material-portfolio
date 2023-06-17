8f56a7d940d5ddca196b98d3ebfdf1bcdd3a43b1
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-17 09:09:10 +0200

    feat: added local storage of darkMode

--- src/app/core/nav/top-navbar/top-navbar.component.ts
+++ src/app/core/nav/top-navbar/top-navbar.component.ts
@@ -1,14 +1,20 @@
-import { Component, Output, EventEmitter, ChangeDetectionStrategy, inject, OnInit } from "@angular/core";
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  inject,
+  OnInit,
+} from "@angular/core";
 import { MatSlideToggleChange } from "@angular/material/slide-toggle";
 import { BreakpointService } from "../../services/breakpoint.service";
 
 @Component({
   selector: "app-top-navbar",
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./top-navbar.component.html",
   styleUrls: ["./top-navbar.component.scss"],
 })
-export class TopNavbarComponent {
+export class TopNavbarComponent implements OnInit {
   breakpointService = inject(BreakpointService);
   columns$ = this.breakpointService.columns$;
   @Output()
@@ -16,11 +22,19 @@ export class TopNavbarComponent {
 
   @Output() public sidenavToggle = new EventEmitter<boolean>();
 
+  ngOnInit(): void {
+    const localMode = localStorage.getItem("darkMode");
+    const isDarkMode: Boolean =
+      localMode == null ? false : JSON.parse(localMode);
+    return this.darkModeSwitched.emit(isDarkMode);
+  }
+
   public onToggleSidenav = (): void => {
     this.sidenavToggle.emit();
   };
 
   onDarkModeSwitched({ checked }: MatSlideToggleChange): void {
+    localStorage.setItem("darkMode", JSON.stringify(checked));
     return this.darkModeSwitched.emit(checked);
   }
 }
