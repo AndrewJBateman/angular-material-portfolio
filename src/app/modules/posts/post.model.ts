3b6fbde807c983c57d1d284d952fff28fbba28e2
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-22 10:36:25 +0200

    feat: add environment as Posts subject

--- src/app/modules/posts/post.model.ts
+++ src/app/modules/posts/post.model.ts
@@ -1,6 +1,6 @@
 export class Post {
   id: number = 0;
-  category: string = 'IT' || 'Dev' || 'Eng';
+  category: string = 'IT' || 'Dev' || 'Eng' || 'Env';
   title: string = '';
   subtitle: string = '';
   content: string = '';
