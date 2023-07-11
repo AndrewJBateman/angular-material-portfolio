785d82724ae05b5f8cc3583cdd853746ac0dd61c
AndrewJBateman <gomezbateman@yahoo.com>
2023-07-11 13:04:35 +0200

    chore: prettier code linting config. added

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { Meta, Title } from "@angular/platform-browser";
 import {
 	FormBuilder,
