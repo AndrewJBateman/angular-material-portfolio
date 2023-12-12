5c0f1baed11fe7eb5c66c747d7e602624bd00825
AndrewJBateman <gomezbateman@yahoo.com>
2023-12-12 22:19:42 +0100

    styles: correct scss url link

--- src/app/shared/components/svg-code/svg-code.component.ts
+++ src/app/shared/components/svg-code/svg-code.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
 	selector: "app-svg-code",
 	templateUrl: "../../../../../src/assets/svgs/code.svg",
-	styleUrls: ["../svg-button.component.scss"],
+	styleUrls: ["../svg.component.scss"],
 })
 export class SvgCodeComponent {}
