c51bbfba61e7adc1d1f4e632eda3cf3452a3830c
AndrewJBateman <gomezbateman@yahoo.com>
2023-12-20 19:33:58 +0100

    refactor: add url to env, add error handling for http call

--- src/app/modules/contact/services/email-contact.service.ts
+++ src/app/modules/contact/services/email-contact.service.ts
@@ -1,21 +1,31 @@
 import { ContactModel } from "./../models/contact.model";
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { catchError } from "rxjs/operators";
+import {throwError } from "rxjs"
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../../../environments/environment";
 
 @Injectable({
 	providedIn: "root",
 })
 export class EmailContactService {
-	formspreeUrl = "https://formspree.io/f/maykykvq";
+  httpClient = inject(HttpClient)
+	formspreeUrl = environment.formspree.FORMSPREE_URL;
 	jsonHeader = new HttpHeaders({ "Content-Type": "application/json" });
 
-	constructor(private readonly http: HttpClient) {}
 
 	sendEmail(form: Partial<ContactModel>) {
-		return this.http.post(
-			this.formspreeUrl,
-			{ name: form.name, replyto: form.email, message: form.message },
-			{ headers: this.jsonHeader }
-		);
+		return this.httpClient
+			.post(
+				this.formspreeUrl,
+				{ name: form.name, replyto: form.email, message: form.message },
+				{ headers: this.jsonHeader }
+			)
+			.pipe(
+				catchError(err => {
+					// Handle error
+					return throwError(() => err);
+				})
+			);
 	}
 }
