e02e2bcd0ee0f12283178a59420a9e0a70b3d80d
AndrewJBateman <gomezbateman@yahoo.com>
2025-07-20 16:19:35 +0200

    chore: dep update to fix error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,21 @@
 export const environment = {
 	production: true,
 	firebase: {
-    "access credentials here"
+    apiKey: "AIzaSyDg8PqDiXDx7CbY59v2PrRGHp9TGvtJsnA",
+    authDomain: "angular-blog-14cef.firebaseapp.com",
+    databaseURL: "https://angular-blog-14cef.firebaseio.com",
+    projectId: "angular-blog-14cef",
+    storageBucket: "angular-blog-14cef.appspot.com",
+    messagingSenderId: "331826448119",
+    appId: "1:331826448119:web:274c208e071ac64eee75f2",
+    measurementId: "G-GP9RVZ6C41"
+
 	},
 	unsplash: {
     API_URL: "https://api.unsplash.com",
-    UNSPLASH_API_KEY: "access credentials here",
+    UNSPLASH_API_KEY: "PAV26ftr4SWQoB9xVIcLS8dXqzxR7le6v6Q_t3Qddq4",
   },
   formspree: {
-    FORMSPREE_URL: "access credentials here"
+    FORMSPREE_URL: "https://formspree.io/f/maykykvq"
   },
 };
